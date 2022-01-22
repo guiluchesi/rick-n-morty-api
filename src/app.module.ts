d755fc35923e8f264058a9838333bec8efc645a0
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-22 12:04:55 -0300

    feat: implements app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { AppController } from './app.controller';
 import { CharactersController } from './characters/characters.controller';
 import { RickNMortyService } from './services/rick-n-morty/rick-n-morty.service';
 
 @Module({
   imports: [],
-  controllers: [CharactersController],
+  controllers: [AppController, CharactersController],
   providers: [RickNMortyService],
 })
 export class AppModule {}
