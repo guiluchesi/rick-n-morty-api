c121e505f7016800a451b1c95a055a8419f455d4
Victor Perin <me@victorperin.ninja>
2022-01-25 13:59:56 -0300

    test(characters/controller): add tests to listCharacters
    Co-authored-by: Guilherme Luchesi <gui.olhenrique@gmail.com>

---
+++ __test__/entities/character.ts
@@ -0,0 +1,20 @@
+export default {
+  id: 1,
+  name: 'Rick',
+  status: 'string',
+  species: 'string',
+  type: 'string',
+  gender: 'string',
+  origin: {
+    name: 'string',
+    url: 'string',
+  },
+  location: {
+    name: 'string',
+    url: 'string',
+  },
+  image: 'string',
+  episode: ['ep1', 'ep2'],
+  url: 'string',
+  created: 'string',
+};
